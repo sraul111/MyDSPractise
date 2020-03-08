7c28d9a917fe086183a97afd1916dc9834674f5e
Sujit Raul <Sujit.Raul@asos.com>
2020-03-08 14:17:35 +0530

    difficult getting solution in c#

--- CloneBinaryTreeMethod1/Program.cs
+++ CloneBinaryTreeMethod1/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO.MemoryMappedFiles;
 
 namespace CloneBinaryTreeMethod1
 {
@@ -20,22 +18,22 @@ namespace CloneBinaryTreeMethod1
 
     public class BinaryTree
     {
-        public void  PrintInOrder(Node node)
+        public void PrintInOrder(Node node)
         {
             if (node == null)
                 return;
 
             PrintInOrder(node.left);
             Console.Write("[" + node.key + " ");
-            if(node.random == null)
+            if (node.random == null)
                 Console.Write("null], ");
             else
-                Console.Write(node.random.key+"], ");
+                Console.Write(node.random.key + "], ");
             PrintInOrder(node.right);
         }
 
         // Function to print the preorder traversal of a binary tree
-        public static void preorder(Node root)
+        public static void Preorder(Node root)
         {
             if (root == null)
             {
@@ -43,41 +41,25 @@ namespace CloneBinaryTreeMethod1
             }
 
             // print data
-            Console.Write(root.key + " -> (");
+            Console.WriteLine(root.key + " -> (");
 
             // print left child's data
-           Console.WriteLine((root.left != null ? root.left.key : "X") + ", ");
+            Console.WriteLine((root.left != null ? Convert.ToString(root.left.key) : "X") + ", ");
 
             // print right child's data
-            System.out.print((root.right != null ? root.right.key : "X") + ", ");
+            Console.WriteLine((root.right != null ? Convert.ToString(root.right.key) : "X") + ", ");
 
             // print random child's data
-            System.out.println((root.random != null ? root.random.data : "X") + ")");
+            Console.WriteLine((root.random != null ? Convert.ToString(root.random.key) : "X") + ")");
 
             // recur for the left and right subtree
-            preorder(root.left);
-            preorder(root.right);
-        }
-        // Recursive function to clone the data, left and right pointers for
-        // each node of a binary tree into a given map
-        public Node CloneLeftRightPointers(Node root, Dictionary<Node ,Node > map)
-        {
-            //base case
-            if (root == null)
-                return null;
-            //clone all fields of the node except the random pointers
-            //create a new node with same data as root node
-            map.Add(root,new Node(root.key));
-            //clone the left and right subtree
-            map.GetValueOrDefault(root).left = CloneLeftRightPointers(root.left, map);
-            map.GetValueOrDefault(root).right = CloneLeftRightPointers(root.right, map);
-            //return cloned root node
-            return map.GetValueOrDefault(root);
+            Preorder(root.left);
+            Preorder(root.right);
         }
 
-       //Recursive function to copy random pointers from the original binary tree
+        //Recursive function to copy random pointers from the original binary tree
         //into the cloned binary using map
-        public static void UpdateRandomPointers(Node root, Dictionary<Node, Node> map)
+        public void UpdateRandomPointers(Node root, Dictionary<Node, Node> map)
         {
             //base case
             if (map.GetValueOrDefault(root) == null)
@@ -85,13 +67,42 @@ namespace CloneBinaryTreeMethod1
             map.GetValueOrDefault(root).random = map.GetValueOrDefault(root.random);
 
             //recur for left and right subtree
-            UpdateRandomPointers(root.left , map);
-            UpdateRandomPointers(root.right ,map );
+            UpdateRandomPointers(root.left, map);
+            UpdateRandomPointers(root.right, map);
         }
-        public static Node CloneSpecialBinaryTree(Node root)
+
+        // Recursive function to clone the data, left and right pointers for
+        // each node of a binary tree into a given map
+        public Node CloneLeftRightPointers(Node root, Dictionary<Node, Node> map)
         {
-            Dictionary<Node, Node> map = new Dictionary<Node,Node>();
-            return Node;
+            //base case
+            if (root == null)
+                return null;
+            //clone all fields of the node except the random pointers
+            //create a new node with same data as root node
+            map.Add(root, new Node(root.key));
+            //clone the left and right subtree
+            map.GetValueOrDefault(root).left = CloneLeftRightPointers(root.left, map);
+            map.GetValueOrDefault(root).right = CloneLeftRightPointers(root.right, map);
+            //return cloned root node
+            return map.GetValueOrDefault(root);
+        }
+
+        //Main function to clone special binary tree with random pointers 
+        public Node CloneSpecialBinaryTree(Node root)
+        {
+            // create a map to store mappings from a node to its clone
+            Dictionary<Node, Node> map = new Dictionary<Node, Node>();
+
+            //clone data left and right pointers for each node of the original
+            //binary tree and put references into the map
+            CloneLeftRightPointers(root, map);
+
+            //update random pointers from  the original binary tree into the map
+            UpdateRandomPointers(root, map);
+
+            //return the cloned root node 
+            return map.GetValueOrDefault(root);
         }
 
     }
@@ -100,7 +111,29 @@ namespace CloneBinaryTreeMethod1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node root = new Node(1);
+            root.left = new Node(2);
+            root.right = new Node(3);
+            root.left.left = new Node(4);
+            root.left.right = new Node(5);
+            root.right.left = new Node(6);
+            root.right.right = new Node(7);
+
+            root.random = root.right.left.random;
+            root.left.left.random = root.right;
+            root.left.right.random = root;
+            root.right.left.random = root.left.left;
+            root.random = root.left;
+
+            BinaryTree binaryTree = new BinaryTree();
+            Console.Write("Preorder traversal of the original tree:");
+            //BinaryTree.Preorder(root);
+            binaryTree.PrintInOrder(root);
+
+            //Node clone = binaryTree.CloneSpecialBinaryTree(root);
+
+            //Console.Write("\nPreorder traversal of the cloned tree:");
+            //BinaryTree.Preorder(clone);
         }
     }
 }
